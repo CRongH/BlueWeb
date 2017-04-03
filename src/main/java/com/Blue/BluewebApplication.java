cb5f76bbf1bfcf6c66c8a66b917de6ddcce5526f
CRH <a453627433@163.com>
2017-04-03 12:31:49 +0800

    SpringBoot Source Code :Hot devtool

---
+++ src/main/java/com/Blue/BluewebApplication.java
@@ -0,0 +1,12 @@
+package com.Blue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluewebApplication.class, args);
+	}
+}
